ceb098cf0a44ee35daddc3275784b7afb8f375b4
harinip04 <32597101+harinip04@users.noreply.github.com>
2018-07-29 14:56:54 +0530

    Update oddeven.java

--- oddeven.java
+++ oddeven.java
@@ -3,7 +3,6 @@ public class oddeven
 {
 	public static void main(String[] args) {
 	       int a=new Scanner(System.in).nextInt();
-	       System.out.print((a%2==0)?"even":"odd");
-		
-	}
+	       System.out.print((a%2==0)?"even":"odd");		
+         	}
 }
