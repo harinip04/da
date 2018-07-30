335e801a513206d91583263d07cf8dcdac0b28a4
harinip04 <32597101+harinip04@users.noreply.github.com>
2018-07-30 18:52:20 +0530

    Create word.java

---
+++ word.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class word
+{
+	public static void main(String[] args) {
+		int a=new Scanner(System.in).nextInt();
+		for(int i=0;i<a;i++)
+		   System.out.println("Hello");
+	}
+}
